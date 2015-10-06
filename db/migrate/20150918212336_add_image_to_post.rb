ac98d243dce0732f637bf31df7a740ee9dca0861
saeedtahir <saeed.tahir48@gmail.com>
2015-10-06 17:03:43 +0500

    testing

--- db/migrate/20150918212336_add_image_to_post.rb
+++ db/migrate/20150918212336_add_image_to_post.rb
@@ -1,7 +1,7 @@
 class AddImageToPost < ActiveRecord::Migration
   def change
-    add_column :posts, :image, :string :remote_image_url
-     belongs_to :posts
+    add_column :posts, :image, :remote_image_url,  :string 
+    belongs_to :posts
 	  # mount_uploader :image, ImageUploader
    #  # mount_uploader :image, Imageuploader
    #  attr_accessible :posts, :name, :image, 
