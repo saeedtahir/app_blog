18190e0bf9921e99db582a3239a852679fbb37e2
Ali <bcsf10a027@gmail.com>
2015-10-15 00:34:28 +0500

    first

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,9 +19,9 @@ Devise.setup do |config|
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
+  require 'devise/orm/active_record'
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
-  require 'devise/orm/active_record'
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
